83b984935415d2e8d7151a1b305e5f062bca6b94
Anderson(Max) <anderson2320@gmail.com>
2015-05-03 03:42:55 -0300

    bug fix

--- response_controller.py
+++ response_controller.py
@@ -4,4 +4,4 @@ import json
 class ResponseController:
   @staticmethod
   def json_response(obj):
-    return Response(json.dump(obj), 200, {'Content-type: text/json'})
+    return Response(json.dumps(obj), 200, {'Content-type: text/json'})
