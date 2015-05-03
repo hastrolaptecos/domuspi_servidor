e7bbdcf748ce9d2b200d09eb2d63658b5a58baa9
Anderson(Max) <anderson2320@gmail.com>
2015-05-03 03:50:02 -0300

    db seed(switchs) + response test

--- db/schema.sql
+++ db/schema.sql
@@ -30,3 +30,19 @@ CREATE TABLE switchs(
 
 -- SEED
 INSERT INTO users(name,login,password) VALUES('ADMIN','domuspi','domuspi');
+
+
+INSERT INTO switchs (label,state,code) VALUES ('Quarto', 0, 0);
+INSERT INTO switchs (label,state,code) VALUES ('Rele 2', 0, 1);
+INSERT INTO switchs (label,state,code) VALUES ('Rele 3', 0, 2);
+INSERT INTO switchs (label,state,code) VALUES ('Rele 4', 0, 3);
+INSERT INTO switchs (label,state,code) VALUES ('Rele 5', 0, 4);
+INSERT INTO switchs (label,state,code) VALUES ('Rele 6', 0, 5);
+INSERT INTO switchs (label,state,code) VALUES ('Rele 7', 0, 6);
+INSERT INTO switchs (label,state,code) VALUES ('Rele 8', 0, 7);
+INSERT INTO switchs (label,state,code) VALUES ('Rele 9', 0, 8);
+INSERT INTO switchs (label,state,code) VALUES ('Rele 10', 0, 9);
+INSERT INTO switchs (label,state,code) VALUES ('12 Volts - GPB2', 0, 10);
+INSERT INTO switchs (label,state,code) VALUES ('12 Volts - GPB3', 0, 11);
+INSERT INTO switchs (label,state,code) VALUES ('12 Volts - GPB4', 0, 12);
+
